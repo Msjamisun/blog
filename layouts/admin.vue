6afcd625a699d7c017b0c190bffdb5ae68a75385
KathrynJamison <kathrynjamison96@gmail.com>
2020-01-24 10:11:05 -0700

    added login page and error layout

---
+++ layouts/admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <AdminHeader />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import AdminHeader from '@/components/admin/AdminHeader'
+export default {
+    components: {
+        AdminHeader
+    }
+};
+</script>
+
+<style>
+html {
+  font-family: "Gelasio", serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="error-page">
+    <h1>What did you do? You broke my site?</h1>
+    <p>Back to <a href="/">home you go...</a></p>
+  </div>
+</template>
+
+<style scoped>
+.error-page {
+  text-align: center;
+}
+
+.error-page a {
+  text-decoration: none;
+  color: red;
+}
+
+.error-page a:hover,
+.error-page a:active {
+  color: salmon;
+}
+</style>
