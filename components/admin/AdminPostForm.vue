e7fb4611092bbf2aafebdbd085257217970dc18a
KathrynJamison <kathrynjamison96@gmail.com>
2020-02-03 09:42:40 -0700

    fixed blog page error

--- components/admin/AdminPostForm.vue
+++ components/admin/AdminPostForm.vue
@@ -45,8 +45,8 @@ export default {
             author: "",
             title: "",
             thumbnail: "",
-            previewText: "",
-            content: ""
+            content: "",
+            previewText: ""
           }
     };
   },
